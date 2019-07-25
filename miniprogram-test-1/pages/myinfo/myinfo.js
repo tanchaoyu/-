bdfe137b7c09147898485623762e89f0d8ef4817
tanchaoyu <1020123290@qq.com>
2019-07-25 13:27:29 +0800

    v 0.34

--- miniprogram-test-1/pages/myinfo/myinfo.js
+++ miniprogram-test-1/pages/myinfo/myinfo.js
@@ -1,66 +1,61 @@
 // pages/myinfo/myinfo.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    infodata: [
+      {
+        time: "2019/7/25",
+        text: "text",
+        reply: true,
+        user: {
+          userlogo: "../../image/ting2.png",
+          username: "ting"
+        }
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    console.log(this.data.infodata);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
